bf5b9e738186d865ce936e49b355cfff788f49c1
ailyfdez <ailyfdez89@gmail.com>
2021-08-12 18:17:07 -0300

    create router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { StarComponent } from './shared/star.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +16,17 @@ import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
+    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    
+    RouterModule.forRoot([
+      { path: 'welcome', component: ProductListComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),    
   ],
   bootstrap: [AppComponent]
 })
