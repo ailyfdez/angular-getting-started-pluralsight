76aba419ae55d3667948cb0d7e3e3f5c6e5acfbb
ailyfdez <ailyfdez89@gmail.com>
2021-08-13 12:03:17 -0300

    add product details guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 
+import { ProductDetailGuard } from "./products/product-detail/product-detail.guard";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,6 +28,7 @@ import { WelcomeComponent } from './home/welcome.component';
     RouterModule.forRoot([
       { path: 'welcome', component:  WelcomeComponent },
       { path: 'products', component:  ProductListComponent },
+      { path: 'products/:id',canActivate:[ProductDetailGuard], component:  ProductDetailComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
     ]),    
